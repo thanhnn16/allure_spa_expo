a0d97eb8409be427baeb48708f1102e3df8009ea
VuNguyen <vunguyen@MBPM1.local>
2024-09-17 14:18:40 +0700

    Create component AppButton, AppTextInput, pages: Login, Register

---
+++ assets/styles/AppStyles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const AppStyles = StyleSheet.create({
+    buttonFill: {
+        backgroundColor: '#717658',
+        borderRadius: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50
+    },
+    buttonOutline: {
+        borderRadius: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        borderColor: '#717658',
+        borderWidth: 1,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)'
+
+    }
+})
