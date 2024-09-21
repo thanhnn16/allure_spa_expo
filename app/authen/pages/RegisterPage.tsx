1304ef200c1cd143d0112cf9578686aa2b174819
vunv <vunv.dev.mobile@gmail.com>
2024-09-21 21:18:38 +0700

    Update/BottomTabs navigations

---
+++ app/authen/pages/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const RegisterPage = () => {
+  return (
+    <View>
+      <Text>Register</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
