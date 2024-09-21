46e11dc1a47d13386a3aedd490fca8e3a93e5f80
vunv <vunv.dev.mobile@gmail.com>
2024-09-21 21:18:38 +0700

    Update/BottomTabs navigations

---
+++ app/(tabs)/ScheduledPage.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ScheduledPage = () => {
+  return (
+    <View>
+      <Text>ScheduledPage</Text>
+    </View>
+  )
+}
+
+export default ScheduledPage
+
+const styles = StyleSheet.create({})
