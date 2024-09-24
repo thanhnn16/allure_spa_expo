b26564253990a51bfe198c3ded73dfae81b05ab5
Nông Nguyễn Thành <thanhnn16.work@gmail.com>
2024-09-24 13:51:44 +0700

    fixup! Update/BottomTabs navigations

--- app/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from 'expo-router';
-
-// This file index.tsx is used to redirect the user to the /(tabs) route
-export default function Index() {
-  return <Redirect href="/(tabs)" />;
-}
