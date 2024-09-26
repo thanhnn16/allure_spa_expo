c2f66bc81034c54c9abb66892e5409eaec59ff04
doquanghieu <quanghieutanlin@gmail.com>
2024-09-26 15:14:48 +0700

    Add component back, send button. edit AppTextInput

--- components/AppTextInput.tsx
+++ components/AppTextInput.tsx
@@ -1,49 +1,71 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import AntDesign from '@expo/vector-icons/AntDesign'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import colors from '@/rn/colors';
 
 export type AppTextInputProps = {
-  title?: string
-  titleStyle?: any
-  textInputStyle?: any
-  containerStyle?: any
-  placeholder?: string
-  value?: string
-  showIcon?: boolean
-  packageIcon?: string
-  iconName?: any
-  iconColor?: string
-  iconSize?: number
-  onPressIcon?: () => void
-  onChangeText?: (text: string) => void
+  title?: string;
+  placeholder?: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
+  titleStyle?: any;
+  textInputStyle?: any;
+  containerStyle?: any;
+  iconColor?: string;
+  iconSize?: number;
+  showIcon?: boolean;
+  onPressIcon?: () => void;
 }
 
 export const AppTextInput = ({
-  title, placeholder, value, onChangeText, titleStyle, textInputStyle, containerStyle,
-  iconName, iconColor, iconSize, showIcon, onPressIcon
+  title,
+  placeholder,
+  value,
+  onChangeText,
+  titleStyle,
+  textInputStyle,
+  containerStyle,
+  iconColor,
+  iconSize,
+  showIcon,
+  onPressIcon
 }: AppTextInputProps) => {
-
   return (
     <View style={containerStyle}>
       <Text style={titleStyle}>{title || 'Title'}</Text>
-      <View style={styles.containerContent}>
-        <TextInput
-          value={value}
-          placeholder={placeholder || 'Placeholder'}
-          onChangeText={onChangeText}
-          style={textInputStyle}
-        />
-        {showIcon && <TouchableOpacity onPress={onPressIcon}>
-          <AntDesign name={iconName || 'questioncircle'} size={iconSize || 24} color={iconColor || 'black'} />
-        </TouchableOpacity>}
+      <View style={{ marginTop: 5 }}>
+        <View style={[styles.inputContainer, { borderColor: colors.primary }]}>
+          <TextInput
+            value={value}
+            placeholder={placeholder || 'Placeholder'}
+            onChangeText={onChangeText}
+            style={[styles.textInput, textInputStyle]}
+          />
+          {showIcon && (
+            <TouchableOpacity onPress={onPressIcon}>
+              <AntDesign name={'questioncircle'} size={iconSize || 24} color={iconColor || 'black'} />
+            </TouchableOpacity>
+          )}
+        </View>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   containerContent: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-  }
-})
+  },
+  inputContainer: {
+    borderWidth: 2, 
+    borderRadius: 8,
+    padding: 8, 
+    borderColor: colors.primary, 
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textInput: {
+    flex: 1,
+  },
+});
