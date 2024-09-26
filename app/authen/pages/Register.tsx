5c0fbf5aa74f5b9ebdf4b7c4701328b90b2a4588
Đỗ Quang Hiếu <166965797+quhie@users.noreply.github.com>
2024-09-26 22:32:59 +0700

    Quanghieu (#21)
    * push demo
    
    * Add empty git file
    
    * Update Login, Register, and add Onboarding page
    
    * Add quanghieu feature for user login
    
    * Add UI Login, OTP, Register, Onboarding
    
    * Add component back, send button. edit AppTextInput
    
    * add font, delete file git
    
    * dele font
    
    * add font, remove file git
    
    ---------
    
    Co-authored-by: doquanghieu <quanghieutanlin@gmail.com>
    Co-authored-by: Nông Nguyễn Thành <thanhnn16.work@gmail.com>

---
+++ app/authen/pages/Register.tsx
@@ -0,0 +1,138 @@
+import React, { useEffect } from 'react';
+import { View, Image, Text, Colors, Spacings } from 'react-native-ui-lib';
+import { ImageBackground } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { NavigationProp } from '@react-navigation/native';
+import AppButton from '@/components/AppButton';
+import { AppTextInput } from '@/components/AppTextInput';
+import SendButton from '@/components/sendButton';
+import BackButton from '@/components/backButton';
+
+SplashScreen.preventAutoHideAsync();
+
+interface RegisterProps {
+  navigation: NavigationProp<any>;
+}
+
+
+const commonInputStyle = {
+  width: 345,
+  height: 45,
+  borderRadius: 8,
+  marginTop: Spacings.s3,
+};
+
+const Register: React.FC<RegisterProps> = ({ navigation }) => {
+  const [fontsLoaded] = useFonts({
+    'AlexBrush-Regular': require('@/assets/fonts/AlexBrush-Regular.ttf'),
+    'OpenSans-Regular': require('@/assets/fonts/OpenSans-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null; 
+  }
+
+  return (
+    <ImageBackground
+      source={require('@/assets/images/authen/img_bg_authen.png')}
+      style={{ flex: 1 }}
+    >
+      <View flex center>
+        <Image
+          width={250}
+          height={85}
+          source={require('@/assets/images/logo/nameAllure.png')}
+        />
+        <Text
+          h2
+          style={{
+            textAlign: 'center',
+            color: Colors.primary,
+            fontFamily: 'AlexBrush-Regular',
+            fontSize: 32,
+            paddingEnd: 50,
+          }}
+        >
+          Nghệ thuật chăm da
+        </Text>
+        <Text
+          h2
+          style={{
+            textAlign: 'center',
+            color: Colors.primary,
+            fontFamily: 'AlexBrush-Regular',
+            fontSize: 32,
+            paddingStart: 50,
+          }}
+        >
+          Từ nghệ nhân Nhật Bản
+        </Text>
+      </View>
+      <View
+        flexS
+        center
+        bg-secondary
+        style={{
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          paddingBottom: 12,
+        }}
+      >
+        <AppTextInput
+          title="Số điện thoại"
+          placeholder="Nhập số điện thoại"
+          containerStyle={{ ...commonInputStyle, marginBottom: 32, marginTop: 42 }}
+          titleStyle={{
+            fontSize: 16,
+            fontFamily: 'OpenSans-Regular',
+            color: Colors.primary,
+          }}
+          textInputStyle={{ height: 30, color: Colors.black }}
+        />
+        <AppTextInput
+          title="Họ và tên"
+          placeholder="Nhập họ và tên"
+          containerStyle={{ ...commonInputStyle, marginBottom: 12, marginTop: 32 }}
+          titleStyle={{
+            fontSize: 16,
+            fontFamily: 'OpenSans-Regular',
+            color: Colors.primary,
+          }}
+          textInputStyle={{ height: 30, color: Colors.black }}
+        />
+        <SendButton
+        title='Gửi mã OTP'
+        />
+        <BackButton
+         title='Quay lại'
+        />
+        <Text
+          center
+          color={Colors.black}
+          marginT-20
+          marginB-100
+          style={{ paddingHorizontal: Spacings.s6 }}
+        >
+          Bằng cách tiếp tục, bạn sẽ đồng ý với{' '}
+          <Text color={Colors.black} style={{ fontWeight: 'bold' }}>
+            Điều khoản sử dụng
+          </Text>{' '}
+          và{' '}
+          <Text color={Colors.black} style={{ fontWeight: 'bold' }}>
+            Chính sách bảo mật
+          </Text>{' '}
+          của chúng tôi
+        </Text>
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default Register;
