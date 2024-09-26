8d9f127aa7dd0512f6e0ca8c125bbbe95d796105
doquanghieu <quanghieutanlin@gmail.com>
2024-09-26 22:28:13 +0700

    add font, remove file git

---
+++ app/authen/pages/index.ts
@@ -0,0 +1,9 @@
+import Onboarding from '../pages/Onboarding';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import OTP from '../pages/OTP';
+
+export { OTP };
+export { Onboarding };
+export { Login };
+export { Register };
