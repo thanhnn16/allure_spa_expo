bbd014534b5d53ee61eba5f34599616c3c64b4f5
ptnzzn <nhwn.drg@gmail.com>
2024-09-29 16:09:13 +0700

    abc xyz

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,31 @@ import { Redirect } from 'expo-router';
 import { Link } from 'expo-router';
 import { View } from 'react-native-ui-lib';
 import ChangeLanguageExample from './example-changeLanguage/change_language_example';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 
 // This file index.tsx is used to redirect the user to the /(tabs) route
 export default function Index() {
+  const [loaded, error] = useFonts({
+    'SFProText-Bold': require('../assets/fonts/SFProText-Bold.otf'),
+    'SFProText-Semibold': require('../assets/fonts/SFProText-Semibold.otf'),
+    'SFProText-Medium': require('../assets/fonts/SFProText-Medium.otf'),
+    'SFProText-Regular': require('../assets/fonts/SFProText-Regular.otf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      console.log('Font loaded');
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   
   return (
-    <Redirect href="/authen/onboarding" /> 
+    // <Redirect href="/authen/onboarding" />
+    <Redirect href="/(tabs)/(home)/" /> 
   )
 }
 
