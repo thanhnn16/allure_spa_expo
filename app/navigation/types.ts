7dc3c893b78760554809bebea6f7654a13abcbe7
Nguyen Duc Loc <129713070+LocndHocCode@users.noreply.github.com>
2024-10-04 01:14:45 +0700

    Locnecon/appicon (#31)
    * locnecon/appicon
    
    Đã làm appicon , đã làm FlashScreen sau 3s chuyern sang onboard , click sang login, register
    
    * address
    
    address
    
    * address
    
    address, nữa phần của Update addresss giờ làm tiếp
    
    * f
    
    g
    
    * taone
    
    lên chauw
    
    ---------
    
    Co-authored-by: Nguyen Duc Loc <129713070+hihihihi252@users.noreply.github.com>
    Co-authored-by: thanhnn16 <thanhnn16.work@gmail.com>

---
+++ app/navigation/types.ts
@@ -0,0 +1,9 @@
+export type RootStackParamList = {
+    FlashScreen: undefined;
+    Onboard: undefined;
+    Login: undefined;
+    Register: undefined;
+    Address: undefined;
+    UpdateAddress: undefined;
+    AddAddress: undefined;
+  };
