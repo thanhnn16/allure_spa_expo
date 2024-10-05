debb396b34197eb4f731cd0fdac3b8c9df96195a
vunv <vunv.dev.mobile@gmail.com>
2024-10-05 11:09:25 +0700

    Delete key

--- commons/weather/index.tsx
+++ commons/weather/index.tsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { Double } from 'react-native/Libraries/Types/CodegenTypes';
 
-const OPENWEATHERMAP_API_KEY = '13b82ae5913353d7c47bb1697c7e90f8'; // Khoá API của tài khoản OpenWeatherMap
+const KEY = ''; 
 
 const getWeather = async (lat: Double, lon: Double) => {
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${OPENWEATHERMAP_API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${KEY}`
     );
     return response.data;
   } catch (error) {
