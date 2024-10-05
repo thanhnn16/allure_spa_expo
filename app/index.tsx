be086d12cc1cfb3e5d024546f2e7e6e9b5ad051b
thanhnn16 <thanhnn16.work@gmail.com>
2024-10-05 17:52:34 +0700

    refactor: Remove unused imports and update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -20,27 +20,8 @@ export default function Index() {
   if (!loaded && !error) {
     return null;
   }
-  
+
   return (
-    <Redirect href="/authen/" /> 
+    <Redirect href="/authen/" />
   )
 }
-
-
-import { Redirect } from 'expo-router';
-import { Link } from 'expo-router';
-import { Colors, View } from 'react-native-ui-lib';
-import ChangeLanguageExample from './example-changeLanguage/change_language_example';
-import { StatusBar } from 'react-native';
-
-// This file index.tsx is used to redirect the user to the /(tabs) route
-export default function Index() {
-  
-  return (
-    <View>
-      <StatusBar backgroundColor={Colors.transparent}/>
-      <Redirect href="/authen/otp" /> 
-    </View>
-  )
-}
-
