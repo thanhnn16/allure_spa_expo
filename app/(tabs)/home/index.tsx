41ee86c020ab4c3c19baf03a45eb4db645aa4596
VuNguyenDevMobile <168943491+VuNguyenDevMobile@users.noreply.github.com>
2024-10-12 17:20:29 +0700

    Vunv (#39)
    * Update HomePage
    
    * Update folder (tabs)
    
    * Update tabs navigations
    
    * Add weather in HomePage
    
    * Delete key
    
    * Restore code HomePage
    
    * Restore (tabs)/_layout.tsx
    
    ---------
    
    Co-authored-by: Nông Nguyễn Thành <131402679+thanhnn16@users.noreply.github.com>
    Co-authored-by: thanhnn16 <thanhnn16.work@gmail.com>

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,11 +1,16 @@
-import { AppStyles } from '@/assets/styles/AppStyles';
+import { AppStyles } from '@/constants/AppStyles';
 import { useEffect, useState } from 'react';
-import { ScrollView } from 'react-native';
+import { FlatList, ScrollView } from 'react-native';
 import { View, Text, Image, SortableList, TouchableOpacity } from 'react-native-ui-lib';
 import getLocation from '@/utils/location/locationHelper';
 import getWeather from '@/utils/weather/getWeatherData';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { Double, Float } from 'react-native/Libraries/Types/CodegenTypes';
+import ButtonNotifyIcon from '@/components/buttons/ButtonNotifyIcon';
+import ButtonMessageIcon from '@/components/buttons/ButtonMessageIcon';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, interpolate, Extrapolation, SharedValue } from 'react-native-reanimated'
+import AppSearch from '@/components/inputs/AppSearch';
+import interpolateScrollY from '@/utils/animated/interpolateScrollY';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { RootStackParamList } from '../types/types'; // Adjust the import path as needed
 
@@ -23,15 +28,65 @@ interface LocationsType {
   name: string;
 }
 
-const HomePage: React.FC = () => {
-  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+interface HeaderStyleParams {
+  offset: SharedValue<number>;
+  height: number;
+}
 
+const HomePage = () => {
   const [cateData, setCateData] = useState<CateItem[]>(cateArr)
-  const [services, setServices] = useState([]);
+  const [services, setServices] = useState<any>([]);
   const [temperature, setTemperature] = useState<Float>(0);
   const [location, setLocation] = useState<LocationsType | null>(null);
   const [weatherIcon, setWeatherIcon] = useState<string>('')
   const [currentDate, setCurrentDate] = useState<string>('')
+
+  const useOffset = useSharedValue(0);
+
+  const headerHideStyle = ({ offset, height }: HeaderStyleParams) =>
+    useAnimatedStyle(() => {
+      const headerHeightAnimated = interpolate(
+        offset.value,
+        [0, height],
+        [height, 0],
+        Extrapolation.CLAMP
+      );
+
+      const headerOpacityAnimated = interpolate(
+        offset.value,
+        [0, height],
+        [1, 0],
+        Extrapolation.CLAMP
+      );
+
+      return {
+        height: withTiming(headerHeightAnimated),
+        opacity: withTiming(headerOpacityAnimated),
+      };
+    }, []);
+
+  const headerShowStyle = ({ offset, height }: HeaderStyleParams) =>
+    useAnimatedStyle(() => {
+      const headerHeightAnimated = interpolate(
+        offset.value,
+        [height, 0],
+        [height, 0],
+        Extrapolation.CLAMP
+      )
+
+      const headerOpacityAnimated = interpolate(
+        offset.value,
+        [height, 0],
+        [1, 0], // Opacity của header khi cuộn
+        Extrapolation.CLAMP
+      );
+
+      return {
+        height: withTiming(headerHeightAnimated),
+        opacity: withTiming(headerOpacityAnimated),
+      };
+    }, []);
+
   useEffect(() => {
     const getProducts = async () => {
       try {
@@ -90,20 +145,13 @@ const HomePage: React.FC = () => {
       </TouchableOpacity>
     )
   }
-  const handleOrderChange = (newData: CateItem[], info: { from: number; to: number }) => {
-    console.log('New order:', newData);
-    console.log('Item moved from index', info.from, 'to index', info.to);
-
-    setCateData(newData);
-
-  };
 
   const renderServiceItem = (item: any) => {
     const rItem = item.item;
     return (
-      <TouchableOpacity marginR-15 style={[AppStyles.shadowItem, { borderRadius: 16 }]} >
+      <TouchableOpacity marginR-15 marginB-5 style={[AppStyles.shadowItem, { borderRadius: 16 }]} >
         <Image source={require('@/assets/images/home/service1.png')} width={250} height={235} />
-        <View paddingH-12 marginT-6 marginB-12>
+        <View paddingH-12 marginT-6>
           <Text text70H>{rItem.name}</Text>
           <Text style={{ color: '#8C8585' }}>{rItem.describe}</Text>
           <Text marginT-10 text70H style={{ color: '#A85A29' }}>{rItem.price + ' VNĐ'}</Text>
@@ -115,9 +163,9 @@ const HomePage: React.FC = () => {
   const renderProductItem = (item: any) => {
     const rItem = item.item;
     return (
-      <TouchableOpacity marginR-15 style={[AppStyles.shadowItem, { borderRadius: 8 }]}>
+      <TouchableOpacity marginR-15 marginB-15 style={[AppStyles.shadowItem, { borderRadius: 8 }]}>
         <Image source={require('@/assets/images/home/product1.png')} width={150} height={180} />
-        <View paddingH-8 paddingB-8 marginT-5>
+        <View paddingH-8 marginT-5>
           <Text text70H>{rItem.name}</Text>
           <View row>
             <View row>
@@ -135,20 +183,28 @@ const HomePage: React.FC = () => {
 
   return (
     <View useSafeArea={true} paddingH-24 center bg-$backgroundDefault >
-      <ScrollView showsVerticalScrollIndicator={false} >
-        <View row left marginB-15 width={345}>
-          <TouchableOpacity onPress={() => navigation.navigate('NotificationPage')}>
-            <Image width={48} height={48} borderRadius={30} source={require('@/assets/images/logo/logo.png')} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('NotNotificationPage')}>
-            <View>
-              <Text text60BO>Đức Lộc</Text>
-              <Text>Allure Spa chúc bạn buổi sáng vui vẻ!</Text>
+      <View row marginT-25 width={345} height={60}>
+        <View >
+          <Animated.View style={headerHideStyle({ offset: useOffset, height: 48 })}>
+            <View row>
+              <Image width={48} height={48} borderRadius={30} source={require('@/assets/images/logo/logo.png')} />
+              <View>
+                <Text text60BO>Đức Lộc</Text>
+                <Text marginT-2>Allure Spa chúc bạn buổi sáng vui vẻ!</Text>
+              </View>
             </View>
-          </TouchableOpacity>
+          </Animated.View>
+          <Animated.View style={headerShowStyle({ offset: useOffset, height: 48 })}>
+            <Text text50BO color='#717658'>Khám phá</Text>
+          </Animated.View>
         </View>
-
-        <View row height={60} centerV style={[AppStyles.shadowItem, { borderRadius: 10 }]} marginB-15 marginH-4 paddingH-5>
+        <View row gap-15 >
+          <ButtonNotifyIcon onPress={() => { alert('Add navigate in line 134') }} />
+          <ButtonMessageIcon onPress={() => { alert('Add navigate in line 135') }} />
+        </View>
+      </View>
+      <Animated.View style={headerHideStyle({ offset: useOffset, height: 60 })}>
+        <View row height={60} width={345} centerV style={[AppStyles.shadowItem, { borderRadius: 10 }]} marginB-15 marginH-4 paddingH-5>
           <View row centerV>
             <Image source={{ uri: `https://openweathermap.org/img/wn/${weatherIcon}@2x.png` }} width={40} height={40} />
             <Text marginL-5 text60>{temperature.toFixed(0)}°C</Text>
@@ -164,16 +220,26 @@ const HomePage: React.FC = () => {
             </View>
           </View>
         </View>
+      </Animated.View>
+
+      <Animated.View style={headerShowStyle({ offset: useOffset, height: 50 })}>
+        <AppSearch />
+      </Animated.View>
+
+      <ScrollView
+        style={{ marginTop: 15 }}
+        showsVerticalScrollIndicator={false}
+        onScroll={(e) => {
+          useOffset.value = e.nativeEvent.contentOffset.y
+        }} >
 
         <Image borderRadius={12} width={345} height={163} source={require('@/assets/images/home/PanerHome.png')} />
 
         <View width={345} height={65} marginT-15>
-          <SortableList
+          <FlatList
             data={cateArr}
             renderItem={renderCateItem}
-            onOrderChange={handleOrderChange}
             keyExtractor={item => item.id.toString()}
-            flexMigration={true}
             horizontal
             showsHorizontalScrollIndicator={false}
           />
@@ -188,12 +254,10 @@ const HomePage: React.FC = () => {
             </TouchableOpacity>
           </View>
 
-          <SortableList
+          <FlatList
             data={services}
             renderItem={renderServiceItem}
-            onOrderChange={handleOrderChange}
             keyExtractor={item => item.id.toString()}
-            flexMigration={true}
             horizontal
             showsHorizontalScrollIndicator={false}
           />
@@ -208,12 +272,10 @@ const HomePage: React.FC = () => {
             </TouchableOpacity>
           </View>
 
-          <SortableList
+          <FlatList
             data={services}
             renderItem={renderProductItem}
-            onOrderChange={handleOrderChange}
             keyExtractor={item => item.id.toString()}
-            flexMigration={true}
             horizontal
             showsHorizontalScrollIndicator={false}
           />
@@ -228,12 +290,10 @@ const HomePage: React.FC = () => {
             </TouchableOpacity>
           </View>
 
-          <SortableList
+          <FlatList
             data={services}
             renderItem={renderProductItem}
-            onOrderChange={handleOrderChange}
             keyExtractor={item => item.id.toString()}
-            flexMigration={true}
             horizontal
             showsHorizontalScrollIndicator={false}
           />
