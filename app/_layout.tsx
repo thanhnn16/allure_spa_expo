1ea5fb45dfe210e92e8dcf36409acbc6b53bad55
thanhnn16 <thanhnn16.work@gmail.com>
2024-10-23 21:59:08 +0700

    Refactor iOS project configuration and add Zalo login feature

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout() {
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="index" options={{ headerShown: false }} />
               <Stack.Screen name="notification/index" options={{ headerShown: false }} />
+              <Stack.Screen name="oauth" options={{ headerShown: false }} />
             </Stack>
           </View>
         </LanguageManager>
