818cac748c15871065c015fb9b2d67fb36339a10
Trần Nguyên Dương <106507835+kennn1205@users.noreply.github.com>
2024-10-28 11:59:23 +0700

    Refactor profile page to use i18n for localization

--- app/(app)/(tabs)/profile/detail.tsx
+++ app/(app)/(tabs)/profile/detail.tsx
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity, Image, Card } from "react-native-ui-lib";
 import { useNavigation } from "expo-router";
 import BackButton from "@/assets/icons/back.svg";
 import ArrowRight from "@/assets/icons/arrow.svg";
-
+import i18n from "@/languages/i18n";
 
 interface ProfileDetailProps {}
 
@@ -22,7 +22,7 @@ const ProfileDetail = (props: ProfileDetailProps) => {
         </TouchableOpacity>
         <View flex center>
           <Text text60 bold marginR-30 style={{ color: "#717658" }}>
-            Hồ Sơ
+            {i18n.t("profile.title")}
           </Text>
         </View>
       </View>
@@ -34,50 +34,51 @@ const ProfileDetail = (props: ProfileDetailProps) => {
           style={{ borderColor: "#D5D6CD", borderWidth: 2 }}
           source={require("@/assets/images/avt.png")}
         />
-        <Text text60>Nguyễn Văn Tèo</Text>
+        <Text text60>{i18n.t("profile.username")}</Text>
         <Text gray>+84 346 542 636</Text>
       </View>
       <Card width={"100%"} marginT-20>
         {[
           {
-            title: "Chỉnh sửa hồ sơ",
+            title: i18n.t("profile.edit_profile"),
             icon: require("@/assets/images/edit.png"),
             onPress: () => {
-                navigation.navigate('ProfileEdit' as never);
-              console.log("Chỉnh sửa hồ sơ"); 
-            }
+              navigation.navigate("ProfileEdit" as never);
+              console.log("Chỉnh sửa hồ sơ");
+            },
           },
           {
-            title: "Đổi mật khẩu",
+            title: i18n.t("profile.change_password"),
             icon: require("@/assets/images/key.png"),
             onPress: () => {
               console.log("đổi mật khẩu");
             },
           },
           {
-            title: "Lịch sử đăng nhập",
+            title: i18n.t("profile.history_login"),
             icon: require("@/assets/images/global.png"),
             onPress: () => {
               console.log("Lịch sử đăng nhập");
             },
           },
-            {
-                title: "Tải thông tin của bạn xuống",
-                icon: require("@/assets/images/dowload.png"),
-                onPress: () => {
-                console.log("download");
-                },
+          {
+            title: i18n.t("profile.dowload_information"),
+
+            icon: require("@/assets/images/dowload.png"),
+            onPress: () => {
+              console.log("download");
             },
-            {
-                title: "Xóa tài khoản",
-                icon: require("@/assets/images/deleteacount.png"),
-                onPress: () => {
-                console.log("xóa tài khoản");
-                },
-            }
+          },
+          {
+            title: i18n.t("profile.delete_account"),
+            icon: require("@/assets/images/deleteacount.png"),
+            onPress: () => {
+              console.log("xóa tài khoản");
+            },
+          },
         ].map((item, index) => (
           <TouchableOpacity key={index} onPress={item.onPress}>
-            <View  row padding-10 gap-20 center>
+            <View row padding-10 gap-20 center>
               <TouchableOpacity
                 style={{
                   width: 50,
