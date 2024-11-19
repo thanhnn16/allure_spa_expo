7f26f9a8f4eaea4338b6a7ca78b97ad0470eed43
Nông Nguyễn Thành <thanhnn16.work@gmail.com>
2024-11-19 09:10:56 +0700

    Add Zalo SDK android dependencies

--- android/app/src/main/java/com/faithnippon/allurespa/MainApplication.kt
+++ android/app/src/main/java/com/faithnippon/allurespa/MainApplication.kt
@@ -40,6 +40,7 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+    ZaloSDKApplication.wrap(this);
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
