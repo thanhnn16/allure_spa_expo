2c30352f70cb76a18e581db2a0877758c43b1c26
Nông Nguyễn Thành <thanhnn16.work@gmail.com>
2024-11-21 21:39:46 +0700

    Refactor rating page to include average rating and total ratings

--- redux/features/rating/getAllRatingProductThunk.ts
+++ redux/features/rating/getAllRatingProductThunk.ts
@@ -2,26 +2,21 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import AxiosInstance from "@/utils/services/helper/axiosInstance";
 import { RatingsListResponseParams } from "@/types/rating.type";
 
-interface RatingRequest {
-  id: string;
-}
-
 export const getAllRatingProductThunk = createAsyncThunk(
   'products/getAllRating',
-  async (body: RatingRequest, { rejectWithValue }: { rejectWithValue: any }) => {
+  async (params: { id: string }, { rejectWithValue }: { rejectWithValue: any }) => {
     try {
-      const res = await AxiosInstance().get<RatingsListResponseParams>(`products/${body.id}/ratings`);
+      const res = await AxiosInstance().get<RatingsListResponseParams>(`products/${params.id}/ratings`);
 
       if (res.data.data) {
-        console.log('Get ratings success:', res.data.data);
-        return res.data.data;
+        return res.data.data.data;
       }
-
-      console.log('Get ratings failed:', res.data.message);
-      return rejectWithValue(res.data.message || 'Get rating failed');
+      return [];
     } catch (error: any) {
-      console.error('Get ratings error:', error);
-      return rejectWithValue(error.response?.data?.message || "Get ratings failed");
+      console.error("Get ratings error:", error.response?.data?.message);
+      return rejectWithValue(
+        error.response?.data?.message || "Get ratings failed"
+      );
     }
   }
 );
